f12a02f3e1052973dd2257f005f0c2f2bc915070
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-01-13 22:14:28 +0100

    original script deleted

--- script.js
+++ script.js
@@ -1,117 +1,167 @@
-// TODO: Create AI for player2
-// GAME LOGIC
+function createPlayer(name, marker) {
+  return {
+    name,
+    marker,
+    score: 0,
+  };
+}
+
 const gameboard = (function () {
   let grid = [
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
   ];
+  function setMark(marker, row, col) {
+    if (grid[row][col] !== undefined) {
+      return;
+    } else {
+      grid[row][col] = marker;
+    }
+  }
+  function resetGrid() {
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        grid[row][col] = undefined;
+      }
+    }
+  }
   return {
     grid,
+    setMark,
+    resetGrid,
   };
 })();
 
-const player = (function (gameboard) {
-  function createPlayer(name, markerSelector) {
+const gameLogic = (function (gameboard) {
+  let player1 = {};
+  let player2 = {};
+  let round = 1;
+  let player1turn = true;
+  let currentPlayer = player1;
+  let newRound = false;
+  function getGameState() {
     return {
-      name,
-      marker: markerSelector === "X" ? "X" : "O",
-      score: 0,
-      markTile: function (row, column) {
-        gameboard.grid[row][column] === undefined
-          ? ((gameboard.grid[row][column] = this.marker),
-            gameUI.updateGameboardUI(),
-            gameLogic.checkWin(this))
-          : null;
-      },
+      round,
+      player1turn,
+      currentPlayer,
+      player1,
+      player2,
     };
   }
 
-  function setPlayer1(name, marker) {
-    player1 = createPlayer(name, marker);
+  function turnDisplay() {
+    return player1turn
+      ? `${gameLogic.player1.name}, it's your turn`
+      : `${gameLogic.player2.name}, it's your turn`;
+  }
+  function switchPlayer() {
+    if (newRound) {
+      newRound = false;
+      player1turn
+        ? (currentPlayer = gameLogic.player1)
+        : (currentPlayer = gameLogic.player2);
+      return;
+    } else {
+      player1turn = !player1turn;
+      player1turn
+        ? (currentPlayer = gameLogic.player1)
+        : (currentPlayer = gameLogic.player2);
+    }
+  }
+  function checkWin(player) {
+    const arr = gameboard.grid;
+    const diagonal1 = [arr[0][0], arr[1][1], arr[2][2]];
+    const diagonal2 = [arr[2][0], arr[1][1], arr[0][2]];
+    const column0 = [arr[0][0], arr[1][0], arr[2][0]];
+    const column1 = [arr[0][1], arr[1][1], arr[2][1]];
+    const column2 = [arr[0][2], arr[1][2], arr[2][2]];
+    if (
+      (arr[0][0] !== undefined && arr[0].every((tile) => tile === arr[0][0])) ||
+      (arr[1][0] !== undefined && arr[1].every((tile) => tile === arr[1][0])) ||
+      (arr[2][0] !== undefined && arr[2].every((tile) => tile === arr[2][0])) ||
+      (column0[0] !== undefined &&
+        column0.every((tile) => tile === column0[0])) ||
+      (column1[0] !== undefined &&
+        column1.every((tile) => tile === column1[0])) ||
+      (column2[0] !== undefined &&
+        column2.every((tile) => tile === column2[0])) ||
+      (diagonal1[0] !== undefined &&
+        diagonal1.every((tile) => tile === diagonal1[0])) ||
+      (diagonal2[0] !== undefined &&
+        diagonal2.every((tile) => tile === diagonal2[0]))
+    ) {
+      gameUI.updateUI();
+      setTimeout(() => {
+        alert(`${player.name} is the winner!`);
+        gameboard.resetGrid(arr);
+        gameUI.updateUI();
+      }, 0);
+      round++;
+      newRound = true;
+      player.score++;
+      round % 2 === 0 ? (player1turn = false) : (player1turn = true);
+    } else if (arr.every((row) => row.every((tile) => tile !== undefined))) {
+      gameUI.updateUI();
+      setTimeout(() => {
+        alert(`It's a tie!`);
+        gameboard.resetGrid(arr);
+        gameUI.updateUI();
+      }, 0);
+      round++;
+      newRound = true;
+      round % 2 === 0 ? (player1turn = false) : (player1turn = true);
+    }
   }
 
-  function setPlayer2(name, marker) {
-    player2 = createPlayer(name, marker);
-  }
+  function playTurn(row, col) {
+    if (gameboard.grid[row][col] !== undefined) {
+      alert("Choose another cell");
+      return;
+    } else {
+      player1turn
+        ? (currentPlayer = gameLogic.player1)
+        : (currentPlayer = gameLogic.player2);
+      gameboard.setMark(currentPlayer.marker, row, col);
 
-  function getPlayer1() {
-    return player1;
+      checkWin(currentPlayer);
+      switchPlayer();
+      gameUI.updateUI();
+    }
   }
-
-  function getPlayer2() {
-    return player2;
-  }
-
   return {
-    createPlayer,
-    setPlayer1,
-    setPlayer2,
-    getPlayer1,
-    getPlayer2,
+    player1,
+    player2,
+    playTurn,
+    switchPlayer,
+    getGameState,
+    turnDisplay,
   };
 })(gameboard);
 
-const gameLogic = (function () {
-  let player1turn = true;
-  let currentPlayer;
-  return {
-    round: 1,
-    player1turn,
-    currentPlayer,
-    resetScoreboard: function () {
-      this.round = 1;
-      gameUI.updateGameboardUI();
-    },
-    resetGameboard: function (arr) {
-      for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-          arr[i][j] = undefined;
-        }
-      }
-      player1turn = true;
-    },
-    checkWin: function (player) {
-      const arr = gameboard.grid;
-      const diagonal1 = [arr[0][0], arr[1][1], arr[2][2]];
-      const diagonal2 = [arr[2][0], arr[1][1], arr[0][2]];
-      const column0 = [arr[0][0], arr[1][0], arr[2][0]];
-      const column1 = [arr[0][1], arr[1][1], arr[2][1]];
-      const column2 = [arr[0][2], arr[1][2], arr[2][2]];
-      if (
-        (arr[0][0] !== undefined &&
-          arr[0].every((tile) => tile === arr[0][0])) ||
-        (arr[1][0] !== undefined &&
-          arr[1].every((tile) => tile === arr[1][0])) ||
-        (arr[2][0] !== undefined &&
-          arr[2].every((tile) => tile === arr[2][0])) ||
-        (column0[0] !== undefined &&
-          column0.every((tile) => tile === column0[0])) ||
-        (column1[0] !== undefined &&
-          column1.every((tile) => tile === column1[0])) ||
-        (column2[0] !== undefined &&
-          column2.every((tile) => tile === column2[0])) ||
-        (diagonal1[0] !== undefined &&
-          diagonal1.every((tile) => tile === diagonal1[0])) ||
-        (diagonal2[0] !== undefined &&
-          diagonal2.every((tile) => tile === diagonal2[0]))
-      ) {
-        console.log(`${player.name} is the winner!`);
-        this.resetGameboard(arr);
-        this.round++;
-        player.score++;
-      } else if (arr.every((row) => row.every((tile) => tile !== undefined))) {
-        console.log("It's a tie!");
-        this.resetGameboard(arr);
-        this.round++;
-      } else {
-        player1turn = !player1turn;
-      }
-    },
-  };
-})();
+const gameUI = (function (gameboard, gameLogic) {
+  let body,
+    mainDiv,
+    gameTitle,
+    roundCounter,
+    scoreboard,
+    player1info,
+    player2info,
+    player1score,
+    player2score,
+    gameboardDisplay,
+    turnDisplay,
+    dialogForm,
+    chooseName1,
+    chooseName2,
+    nameInput,
+    markerDiv,
+    markerP,
+    playerDialog,
+    xOption,
+    oOption,
+    playButton;
 
-const gameUI = (function (gameboard, player, gameLogic) {
   function createDOMElement(type, attributes, text) {
     const element = document.createElement(type);
 
@@ -123,37 +173,34 @@ const gameUI = (function (gameboard, player, gameLogic) {
 
     return element;
   }
-// TODO: resolve why no listener is being added, check all player.players to change them to the global player and check whether its marktile func works
-  function cellClicked(row, col) {
-    if (!player1 || !player2) {
-      return;
-    }
-    gameLogic.player1turn
-      ? player1.markTile(row, col)
-      : player2.markTile(row, col);
-  }
 
-  function startGame() {
-    const body = document.querySelector("body");
-    const mainDiv = gameUI.createDOMElement("div", { class: "main" });
-    const gameTitle = gameUI.createDOMElement(
-      "h1",
-      { id: "title" },
-      "TIC TAC TOE"
-    );
-    const roundCounter = gameUI.createDOMElement("div", {
+  function initUI() {
+    // 1) Add listeners to interact with logic ALWAYS CREATE ALL UI -> THEN APPEND -> THEN MODIFY CONTENT ACCORDING TO GAME STATE
+    body = document.querySelector("body");
+    body.innerHTML = "";
+    mainDiv = gameUI.createDOMElement("div", { class: "main" });
+    gameTitle = gameUI.createDOMElement("h1", { id: "title" }, "TIC TAC TOE");
+    roundCounter = gameUI.createDOMElement("div", {
       id: "round_counter",
     });
-    const scoreboard = gameUI.createDOMElement("div", { id: "scoreboard" });
-    const player1score = gameUI.createDOMElement("div", {
+    scoreboard = gameUI.createDOMElement("div", { id: "scoreboard" });
+    player1info = gameUI.createDOMElement("div", {
+      id: "player1info",
+      class: "playerinfo",
+    });
+    player2info = gameUI.createDOMElement("div", {
+      id: "player2info",
+      class: "playerinfo",
+    });
+    player1score = gameUI.createDOMElement("div", {
       id: "player1score",
       class: "score_display",
     });
-    const player2score = gameUI.createDOMElement("div", {
+    player2score = gameUI.createDOMElement("div", {
       id: "player2score",
       class: "score_display",
     });
-    const gameboardDisplay = gameUI.createDOMElement("div", {
+    gameboardDisplay = gameUI.createDOMElement("div", {
       id: "gameboardDisplay",
     });
     for (let row = 0; row < 3; row++) {
@@ -164,36 +211,39 @@ const gameUI = (function (gameboard, player, gameLogic) {
         });
         cell.dataset.row = row;
         cell.dataset.col = col;
+        cell.addEventListener("click", () => {
+          gameLogic.playTurn(cell.dataset.row, cell.dataset.col);
+        });
         gameboardDisplay.append(cell);
       }
     }
-    const turnDisplay = gameUI.createDOMElement("div", {
+    turnDisplay = gameUI.createDOMElement("div", {
       id: "turn_display",
     });
-    const playerDialog = gameUI.createDOMElement("dialog", {
+    playerDialog = gameUI.createDOMElement("dialog", {
       id: "player_dialog",
     });
-    const dialogForm = gameUI.createDOMElement("form");
-    const chooseName1 = gameUI.createDOMElement(
+    dialogForm = gameUI.createDOMElement("form");
+    chooseName1 = gameUI.createDOMElement(
       "p",
       { id: "choose_name_1" },
       "Enter your name, player 1:"
     );
-    const chooseName2 = gameUI.createDOMElement(
+    chooseName2 = gameUI.createDOMElement(
       "p",
       { id: "choose_name_2" },
       "Enter your name, player 2:"
     );
     chooseName1.setAttribute("required", "");
     chooseName2.setAttribute("required", "");
-    const nameInput = gameUI.createDOMElement("input", {
+    nameInput = gameUI.createDOMElement("input", {
       type: "text",
       id: "name_input",
     });
-    const markerDiv = gameUI.createDOMElement("div", {
+    markerDiv = gameUI.createDOMElement("div", {
       id: "marker_selection",
     });
-    const markerP = gameUI.createDOMElement("p", {}, "Choose your marker: ");
+    markerP = gameUI.createDOMElement("p", {}, "Choose your marker: ");
     function handleClick(e) {
       e.preventDefault();
       e.target.classList.add("selected_marker");
@@ -205,19 +255,19 @@ const gameUI = (function (gameboard, player, gameLogic) {
       e.preventDefault();
       alert("Your marker is already chosen");
     }
-    const xOption = gameUI.createDOMElement(
+    xOption = gameUI.createDOMElement(
       "button",
       { id: "X", class: "marker_button" },
       "X"
     );
     xOption.addEventListener("click", handleClick);
-    const oOption = gameUI.createDOMElement(
+    oOption = gameUI.createDOMElement(
       "button",
       { id: "O", class: "marker_button" },
       "O"
     );
     oOption.addEventListener("click", handleClick);
-    const playButton = gameUI.createDOMElement("button", {
+    playButton = gameUI.createDOMElement("button", {
       id: "play_button",
       title: "Play!",
     });
@@ -233,7 +283,7 @@ const gameUI = (function (gameboard, player, gameLogic) {
       playButton
     );
     playerDialog.append(dialogForm);
-    scoreboard.append(player1score, player2score);
+    scoreboard.append(player1info, player1score, player2score, player2info);
 
     mainDiv.append(
       gameTitle,
@@ -245,19 +295,35 @@ const gameUI = (function (gameboard, player, gameLogic) {
 
     body.append(playerDialog, mainDiv);
 
-    // PLAYER CREATION BEGINS
+    return {
+      handleClick,
+      disableMarker,
+      body,
+      mainDiv,
+      gameTitle,
+      roundCounter,
+      scoreboard,
+      player1info,
+      player2info,
+      player1score,
+      player2score,
+      gameboardDisplay,
+      turnDisplay,
+      dialogForm,
+      chooseName1,
+      chooseName2,
+      nameInput,
+      markerDiv,
+      markerP,
+      playerDialog,
+      xOption,
+      oOption,
+      playButton,
+    };
+  }
+  function createPlayers() {
     playerDialog.showModal();
-    function switchPlayer() {
-      return gameLogic.player1turn
-        ? ((chooseName2.style.display = "none"),
-          (chooseName1.style.display = "block"),
-          (currentPlayer = true))
-        : ((chooseName2.style.display = "block"),
-          (chooseName1.style.display = "none"),
-          (currentPlayer = false));
-    }
-    switchPlayer();
-
+    chooseName2.style.display = "none";
     playButton.addEventListener("click", (e) => {
       e.preventDefault();
       if (nameInput.value === "") {
@@ -269,70 +335,66 @@ const gameUI = (function (gameboard, player, gameLogic) {
         alert("Please choose a marker");
       }
       const selectedMarker = playerDialog.querySelector(".selected_marker");
-      if (gameLogic.player1turn) {
-        player.setPlayer1(nameInput.value, selectedMarker.id);
-        player1 = player.getPlayer1();
-        xOption.removeEventListener("click", handleClick);
-        oOption.removeEventListener("click", handleClick);
+      if (chooseName2.style.display === "none") {
+        gameLogic.player1 = createPlayer(nameInput.value, selectedMarker.id);
+        uiElements.player1score.style.backgroundColor =
+          window.getComputedStyle(selectedMarker).backgroundColor;
+        uiElements.player1score.textContent = `${gameLogic.player1.score}`;
+        uiElements.player1info.textContent = `${gameLogic.player1.name}: ${gameLogic.player1.marker}`;
+        xOption.removeEventListener("click", uiElements.handleClick);
+        oOption.removeEventListener("click", uiElements.handleClick);
         xOption.classList.contains("selected_marker")
           ? (oOption.classList.add("selected_marker"),
             xOption.classList.remove("selected_marker"))
           : (xOption.classList.add("selected_marker"),
             oOption.classList.remove("selected_marker"));
-        xOption.addEventListener("click", disableMarker);
-        oOption.addEventListener("click", disableMarker);
-        gameLogic.player1turn = false;
+        xOption.addEventListener("click", uiElements.disableMarker);
+        oOption.addEventListener("click", uiElements.disableMarker);
         nameInput.value = "";
-        switchPlayer();
+        nameInput.focus();
+        chooseName2.style.display = "block";
+        chooseName1.style.display = "none";
       } else {
-        player.setPlayer2(nameInput.value, selectedMarker.id);
-        player2 = player.getPlayer2();
-        gameLogic.player1turn = true;
-        xOption.addEventListener("click", handleClick);
-        oOption.addEventListener("click", handleClick);
-        xOption.removeEventListener("click", disableMarker);
-        oOption.removeEventListener("click", disableMarker);
+        gameLogic.player2 = createPlayer(nameInput.value, selectedMarker.id);
+        uiElements.player2score.style.backgroundColor =
+          window.getComputedStyle(selectedMarker).backgroundColor;
+        uiElements.player2score.textContent = `${gameLogic.player2.score}`;
+        uiElements.player2info.textContent = `${gameLogic.player2.name}: ${gameLogic.player2.marker}`;
+        xOption.addEventListener("click", uiElements.handleClick);
+        oOption.addEventListener("click", uiElements.handleClick);
+        xOption.removeEventListener("click", uiElements.disableMarker);
+        oOption.removeEventListener("click", uiElements.disableMarker);
         playerDialog.close();
         alert("START!");
+        gameUI.updateUI();
       }
     });
   }
 
-  function updateGameboardUI() {
-    if (!player.player1 || !player.player2) {
-      return;
-    }
+  function updateUI() {
+    roundCounter.textContent = `Round: ${gameLogic.getGameState().round}`;
+    player1score.textContent = `${gameLogic.player1.score}`;
+    player2score.textContent = `${gameLogic.player2.score}`;
+    // add logic for wins and ties
+    turnDisplay.textContent = gameLogic.turnDisplay();
     for (let row = 0; row < 3; row++) {
       for (let col = 0; col < 3; col++) {
-        const gridCell = gameboard.grid[row][col];
-        const UIcell = document.querySelector(
-          `.grid_cell[data-row='${row}'][data-col='${col}']`
-        );
-        UIcell.textContent = gridCell || "";
+        document.querySelector(`#row${row}col${col}`).textContent =
+          gameboard.grid[row][col];
+        document.querySelector(`#row${row}col${col}`).textContent === "X"
+          ? (document.querySelector(`#row${row}col${col}`).style.color = "red")
+          : (document.querySelector(`#row${row}col${col}`).style.color =
+              "black");
       }
     }
-
-    roundCounter.textContent = gameLogic.round;
-    player1score = player.player1.score;
-    player2score = player.player2.score;
-
-    gameLogic.player1turn
-      ? (turnDisplay = `${player.player1.name}, it's your turn`)
-      : (turnDisplay = `${player.player2.name}, it's your turn`);
   }
-
   return {
     createDOMElement,
-    startGame,
-    updateGameboardUI,
-    cellClicked,
+    initUI,
+    createPlayers,
+    updateUI,
   };
-})(gameboard, player, gameLogic);
+})(gameboard, gameLogic);
 
-let player1;
-let player2;
-gameUI.startGame();
-const gridCells = document.querySelectorAll(".grid-cell")
-gridCells.forEach(cell => {
-  cell.addEventListener("click", gameUI.cellClicked(`${cell.dataset.row}`, `${cell.dataset.col}`));
-});
+const uiElements = gameUI.initUI();
+gameUI.createPlayers();
